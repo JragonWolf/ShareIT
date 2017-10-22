730818a5f73021b45128aa5a8fb73030f80f9b1a
Alexander Nestler <anestler2012@gmail.com>
2017-10-22 13:00:38 -0700

    master page added

---
+++ ShareIT/MasterPage/ShareIT.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShareIT.MasterPage
+{
+    public partial class ShareIT : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
